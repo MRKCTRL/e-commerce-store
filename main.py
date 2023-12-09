60f743e1de926f3fbc758c373f0ca54c99817d72
MRKCTRL <davidmbundu@gmail.com>
2023-12-09 13:59:54 +0200

    added django webframeowrk to the project

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-from flask import flask 
+from flask import flask, jsonify, requests, render_template
+import smptlib
+
+
+app = flask.Flask(__name__)
+
+@app.route('/')
+def home():
+    return return_render('index.html')
+
+@app.route('/payment', method=['GET', 'POST'])
+def payment():
+    return render_template('card.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
